9a19df960a018729daf496414f6e48a593275d11
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2021-08-24 19:27:56 +0200

    Continue

--- generator/src/index.ts
+++ generator/src/index.ts
@@ -2,21 +2,45 @@
 
 import * as fs from 'fs';
 import ejs from 'ejs';
+import yargs from 'yargs/yargs';
+
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv))
+	.help()
+	.options({
+			o: {
+				alias: 'output',
+				default: 'src/App.js',
+				description: 'the React output'
+			},
+			i: {
+				alias: 'input',
+				default: 'dtdl.json',
+				description: 'the JSON file containing the DTDL model'
+			}
+		}).parseSync();
+
+console.log(argv.output);
 
 const isTelemetry = (item: any) => {
 	const type = item['@type'];
-	console.log(type);
 	return type === 'Telemetry' || (Array.isArray(type) && type[0] === 'Telemetry');
 };
 
 async function main() {
-	console.log('Hello world');
-	const model = JSON.parse(fs.readFileSync('./Thermostat.json', { encoding: 'utf-8' }));
-	console.dir(model);
+	console.log(`Generating the React file ${argv.output} from the DTDL model ${argv.input}`);
+	const model = JSON.parse(fs.readFileSync(argv.input as string, { encoding: 'utf-8' }));
 
 	const telemetryItems = model.contents.filter((item: any) => isTelemetry(item)); // map((contentsItem: any) => console.log(contentsItem));
-	console.dir(telemetryItems);
-	console.log(await ejs.renderFile('test.ejs', {}, {}));
+	const output = await ejs.renderFile('../template-ui/App.ejs', {telemetryItems: [
+			{
+				telemetryKey: 'temperature'
+			}
+		]}, {});
+
+	fs.writeFileSync(argv.output as string, output);
+	argv.output
 }
 
 main().catch(err => {
