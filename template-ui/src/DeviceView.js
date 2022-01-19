b19f181f91927d9882efd1646766c22fda332b96
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2022-01-19 10:19:34 +0100

    Upgrade to current ux4iot-react version

--- template-ui/src/DeviceView.js
+++ template-ui/src/DeviceView.js
@@ -1,15 +1,9 @@
 import React from 'react';
-//import * as ux4iot from "ux4iot-react";
-import * as ux4iot from "./fake-ux4iot.js";
+import {Ux4iotContextProvider, useTelemetry} from "ux4iot-react";
 import 'semantic-ui-css/semantic.css'
 import { Statistic } from 'semantic-ui-react'
 
 const UX4IOT_ADMIN_CONNECTION_STRING = "<YOUR ADMIN CONNECTION STRING";
-const ux4iotClient = ux4iot.initDevMode(UX4IOT_ADMIN_CONNECTION_STRING);
-
-const useTelemetry = () => {
-  return 42.0
-}
 
 const TelemetryValue = props => {
   const value = useTelemetry(props.deviceId, props.telemetryKey);
@@ -30,11 +24,12 @@ function App() {
 
   return (
     <div className="App">
-      <ux4iot.Context.Provider value={ux4iotClient}>
+      <Ux4iotContextProvider
+        options={{ adminConnectionString: UX4IOT_ADMIN_CONNECTION_STRING }}>
         <DeviceView />
-      </ux4iot.Context.Provider>
+      </Ux4iotContextProvider>
     </div>
   );
 }
 
-export default DeviceView;
+export default App;
